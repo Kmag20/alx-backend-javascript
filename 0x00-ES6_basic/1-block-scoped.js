f2b30c2391bd51f4dddfc7e7658c448f8d0c3c87
Kmag20 <owenmawira@gmail.com>
2024-09-30 23:34:23 +0300

    block scope

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+export default function taskBlock(trueOrFalse) {
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    task = task;
+    task2 = task2;
+  }
+
+  return [task, task2];
+}
+
